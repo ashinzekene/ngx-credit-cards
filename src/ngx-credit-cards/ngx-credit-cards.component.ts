e4b4b743f64687fdf520729eaaea6f50fb39a235
ashinzekene <ashinzekene@gmail.com>
2018-02-10 23:23:54 +0100

    added component

---
+++ src/ngx-credit-cards/ngx-credit-cards.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-credit-cards',
+  templateUrl: 'ngx-credit-cards.component.html'
+})
+export class NgxCreditCardsComponent implements OnInit {
+  constructor() { }
+
+  formatText() {
+    
+  }
+
+  formatNumber() {
+
+  }
+
+  formatCVC() {
+
+  }
+
+  ngOnInit() { }
+}
