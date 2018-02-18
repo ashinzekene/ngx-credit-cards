80a9fa352e4a693256ac3e33266d84e401f1d999
ashinzekene <ashinzekene@gmail.com>
2018-02-19 00:48:58 +0100

    moved styles to component.css

--- examples/src/app/ngx-credit-cards/ngx-credit-cards/ngx-credit-cards.component.ts
+++ examples/src/app/ngx-credit-cards/ngx-credit-cards/ngx-credit-cards.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgxCreditCardsService } from "../ngx-credit-cards.service";
 import { padEnd } from "../utils";
+import { images } from "../card-images";
 
 @Component({
   selector: 'ngx-credit-cards',
+  styleUrls: ['ngx-credit-cards.component.css'],
   templateUrl: 'ngx-credit-cards.component.html'
 })
 export class NgxCreditCardsComponent implements OnInit {
+  public images: any
   public cvv: string = ''
   public name: string = ''
   public number: string = ''
   public expiry: string = this.ngxccService.expiry
-  public type: string = 'unknown'
+  public type: string = 'mastercard'
   public length: number = 16
   @Input() hideName: false
   @Input() focused: string = null
@@ -34,6 +37,7 @@ export class NgxCreditCardsComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.images = images
     this.hideName = this.hideName || false
     this.namePlaceholder = this.namePlaceholder || 'FULL NAME'
     this.expiryBeforeText = this.expiryBeforeText || 'month/year'
